5f9840b49637953ea7a3a2ae9c607655954d5d41
John <jmjohnmcgovern707@gmail.com>
2025-01-22 15:52:45 -0800

    new menu items

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import Logo from './logo';
 import classes from './main-navigation.module.css';
-
+//
 function MainNavigation() {
   return (
     <header className={classes.header}>
