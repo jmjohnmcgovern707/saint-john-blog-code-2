54239ae69abc87fc94e1cb6f1ddbfb2223ee1391
John <jmjohnmcgovern707@gmail.com>
2025-01-24 09:47:51 -0800

    yes

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-  return <div className={classes.logo}>Saint John's Blog</div>;
+  return <div className={classes.logo}>Saint John's Pages</div>;
 }
 
 export default Logo;
